4b716765459fcb11e216cec3dd2f6f7bc52e267b
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-06-23 17:01:32 +0200

    Logging OTL

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OpenTelemetry;
 using Blazor;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,6 +19,24 @@ builder.Services.AddOidcAuthentication(options =>
     // For more information, see https://aka.ms/blazor-standalone-auth
     builder.Configuration.Bind("Auth0", options.ProviderOptions);
     options.ProviderOptions.ResponseType = "code";
+});
+
+// var tracerProvider = Sdk.CreateTracerProviderBuilder()
+//     .AddSource("Balzor")
+//     .AddHttpClientInstrumentation()
+//     //.AddOtlpExporter()
+//     //.AddInMemoryExporter(exportedItems)
+//     .AddConsoleExporter()
+//     .Build();
+  
+builder.Logging.AddOpenTelemetry(logging =>
+{
+    var resourceBuilder = ResourceBuilder
+        .CreateDefault()
+        .AddService("Balzor");
+
+    logging.SetResourceBuilder(resourceBuilder)
+        .AddConsoleExporter();
 });
 
 await builder.Build().RunAsync();
