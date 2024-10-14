a4553c082325d01513f9d8cead8376f8ced5c00e
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-10-14 13:31:54 +0200

    bufferValue

--- blazor/KV.cs
+++ blazor/KV.cs
@@ -1,8 +1,5 @@
-public class KV
+public class KV<T>
 {
-    public string Key { get; set; } 
-    public string Value { get; set; }
-    public string? StringValue { get; set; }
-    public Stream? StreamValue { get; set; }
-    public byte[]? BufferValue { get; set; }    
+    public string? Key { get; set; } 
+    public T? Value { get; set; } 
 }
