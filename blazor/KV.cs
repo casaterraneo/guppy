c87da403eba202599aa21a123c2cbc1b7af776e1
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-10-14 12:26:42 +0200

    InputFile

--- blazor/KV.cs
+++ blazor/KV.cs
@@ -2,4 +2,7 @@ public class KV
 {
     public string Key { get; set; } 
     public string Value { get; set; }
+    public string? StringValue { get; set; }
+    public Stream? StreamValue { get; set; }
+    public byte[]? BufferValue { get; set; }    
 }
