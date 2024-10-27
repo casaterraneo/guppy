c2e9830df821907e9b0ae437bc9830c4b3db1066
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-10-27 10:48:39 +0100

    DOTNET_ENVIRONMENT

--- blazor/build.sh
+++ blazor/build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+# Scarica e imposta dotnet
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
+
+# Mostra la versione di dotnet installata
 ./dotnet/dotnet --version
+
+# Mostra il valore della variabile di ambiente DOTNET_ENVIRONMENT
+echo "DOTNET_ENVIRONMENT: $DOTNET_ENVIRONMENT"
+
+# Pubblica l'app con dotnet
 ./dotnet/dotnet publish -c Release -o output
