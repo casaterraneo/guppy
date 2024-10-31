b2e97d35fba65e8d25b71b5f18aeed1bb26c0b9b
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-10-31 14:55:06 +0100

    _headers DOTNET_ENVIRONMENT

--- blazor/build.sh
+++ blazor/build.sh
@@ -10,5 +10,16 @@ chmod +x dotnet-install.sh
 # Mostra il valore della variabile di ambiente DOTNET_ENVIRONMENT
 echo "DOTNET_ENVIRONMENT: $DOTNET_ENVIRONMENT"
 
+# Seleziona il file _headers in base all'ambiente
+if [ "$DOTNET_ENVIRONMENT" = "Production" ]; then
+    cp ./wwwroot/_headers_Production ./wwwroot/_headers
+elif [ "$DOTNET_ENVIRONMENT" = "Staging" ]; then
+    cp ./wwwroot/_headers_Staging ./wwwroot/_headers
+elif [ "$DOTNET_ENVIRONMENT" = "Development" ]; then
+    cp ./wwwroot/_headers_Development ./wwwroot/_headers
+else
+    echo "Ambiente non riconosciuto, usando file _headers di default"
+fi
+
 # Pubblica l'app con dotnet
 ./dotnet/dotnet publish -c Release -o output -p:EnvironmentName=$DOTNET_ENVIRONMENT
