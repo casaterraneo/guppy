f27ea04080a066892906ca08d1c6ac5a002dd8a1
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-11-03 10:27:27 +0100

    fix build

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { logger } from 'hono/logger'
 import { createRemoteJWKSet, jwtVerify } from 'jose'
-import { employee } from './employee';
+import employee from './employee';
 
 type Bindings = {
 	KV: KVNamespace;
