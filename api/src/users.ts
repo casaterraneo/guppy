5ee7e5459c1c1276bb6c712bb6c0ca925602b543
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-11-16 12:20:54 +0100

    log

--- api/src/users.ts
+++ api/src/users.ts
@@ -28,6 +28,8 @@ const app = new Hono()
 
 	const tokenData = await tokenResponse.json();
 
+	console.log(`tokenData ${tokenData}`);
+
 	// Verifica che l'access token sia stato ottenuto
 	if (!tokenData.access_token) {
 		console.log('Failed to retrieve access token');
@@ -36,8 +38,9 @@ const app = new Hono()
 
 	console.log('Access token ok');
 	const { access_token } = tokenData.access_token;
-
 	console.log(`Bearer ${access_token}`);
+	console.log(`Bearer 2 ${tokenData.access_token}`);
+
 	// Now call the Management API to get user info
 	const userResponse = await fetch('https://dev-lnkfyfu1two0vaem.us.auth0.com/api/v2/users', {
 	  method: 'GET',
