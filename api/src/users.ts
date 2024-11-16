54451fef73d7c2cd6f55a7a092fcd43f8916e1db
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-11-16 10:25:15 +0100

    log

--- api/src/users.ts
+++ api/src/users.ts
@@ -6,10 +6,10 @@ const app = new Hono()
 	if (!userId) return c.json({ error: 'UserId is required' }, 400);
 
 	// Variabili di configurazione
-	const authUrl = 'https://dev-lnkfyfu1two0vaem.us.auth0.com/oauth/token';
+	const authUrl = `https://${c.env.AUTH0_DOMAIN}/oauth/token`;
 	const clientId = '1hirdGQO6H34hzXqPXDVLbq5d1xaU93K'; //Auth0 Management API (Test Application)
 	const clientSecret = c.env.AUTH0_CLIENT_SECRET; // Variabile d'ambiente per il client secret
-	const audience = 'https://dev-lnkfyfu1two0vaem.us.auth0.com/api/v2/';
+	const audience = `https://${c.env.AUTH0_DOMAIN}/api/v2/`;
 
 	// Prima chiamata per ottenere l'access token
 	const tokenParams = new URLSearchParams();
@@ -18,15 +18,19 @@ const app = new Hono()
 	tokenParams.append('client_secret', clientSecret);
 	tokenParams.append('audience', audience);
 
+	console.log('tokenParams :' + tokenParams.toString());
+
 	const tokenResponse = await fetch(authUrl, {
 		method: 'POST',
 		headers: {
-		'content-type': 'application/x-www-form-urlencoded',
+		'content-type': 'application/json',
 		},
 		body: tokenParams.toString(),
 	});
 
 	const tokenData = await tokenResponse.json();
+
+	console.log('tokenData: ' + tokenData);
 
 	// Verifica che l'access token sia stato ottenuto
 	if (!tokenData.access_token) {
@@ -34,6 +38,7 @@ const app = new Hono()
 		return c.json({ error: 'Failed to retrieve access token' }, 400);
 	}
 
+	console.log('Access token ok');
 	const { access_token } = tokenData.access_token;
 
 	// Now call the Management API to get user info
