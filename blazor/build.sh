5696b92a7b0a188cf1cefb5d738b44f7f37de858
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-11-22 22:36:56 +0100

    dotnet 9

--- blazor/build.sh
+++ blazor/build.sh
@@ -2,7 +2,7 @@
 # Scarica e imposta dotnet
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
+./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
 
 # Mostra la versione di dotnet installata
 ./dotnet/dotnet --version
