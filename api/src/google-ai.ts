aa18782b2d503c9d20cd5a34d3b6e4c51e2b3048
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-12-01 20:09:41 +0100

    CleanResponseText

--- api/src/google-ai.ts
+++ api/src/google-ai.ts
@@ -6,29 +6,11 @@ const app = new Hono()
 	const { message } = await c.req.json();
 	if (!message) return c.json({ error: 'Message is required' }, 400);
 
-	console.log(message);
-	//console.log(c.env.CF_AIG_TOKEN);
 	const customHeaders = {
 		'cf-aig-authorization': `Bearer ${c.env.CF_AIG_TOKEN}`
 	};
 
 	const genAI = new GoogleGenerativeAI(c.env.GOOGLE_AI_STUDIO_TOKEN);
-	// const model = genAI.getGenerativeModel(
-	// 	{
-	// 		model: "gemini-1.5-flash",
-	// 	},
-	// 	{
-	// 		baseUrl: 'https://gateway.ai.cloudflare.com/v1/fe04af051f86d0ff6f22622b45242473/guppy-ai-gateway/google-ai-studio',
-	// 		headers: {
-	// 			'cf-aig-authorization': `Bearer ${c.env.CF_AIG_TOKEN}`,
-	// 		  },
-	// 	},
-	// 	// {
-	// 	// 	customHeaders : {
-	// 	// 		 	'cf-aig-authorization': `Bearer ${c.env.CF_AIG_TOKEN}`,
-	// 	// 		   },
-	// 	// }
-	// );
 	const model = genAI.getGenerativeModel({
 		model: "gemini-1.5-flash",
 		baseUrl: 'https://gateway.ai.cloudflare.com/v1/fe04af051f86d0ff6f22622b45242473/guppy-ai-gateway/google-ai-studio',
