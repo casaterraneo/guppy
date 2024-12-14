6db141c90b6caff7a0d50acf3bf6ac03e0eafe80
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-12-14 16:29:28 +0100

    c.env

--- api/src/workers-ai.ts
+++ api/src/workers-ai.ts
@@ -6,7 +6,7 @@ const app = new Hono()
 	const { messages, temperature, topK, topP, maxOutputTokens, responseMimeType, responseSchema } = await c.req.json();
 	if (!messages) return c.json({ error: 'Message is required' }, 400);
 
-	const answer = await env.AI.run('@cf/meta/llama-3.1-8b-instruct', {
+	const answer = await c.env.AI.run('@cf/meta/llama-3.1-8b-instruct', {
 		prompt: messages[0]
 	});
 	return c.json(answer);
