38ceb4b650ae02b86e8489fc80a28165b7c1095a
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-12-14 18:37:42 +0100

    vector

--- api/src/workers-ai.ts
+++ api/src/workers-ai.ts
@@ -1,15 +1,36 @@
 import { Hono } from 'hono';
 
 const app = new Hono()
-.post('/', async (c) => {
+.post('/llm', async (c) => {
 
 	const { messages, temperature, topK, topP, maxOutputTokens, responseMimeType, responseSchema } = await c.req.json();
 	if (!messages) return c.json({ error: 'Message is required' }, 400);
 
 	const answer = await c.env.AI.run('@cf/meta/llama-3.1-8b-instruct', {
-		 prompt: "What is the origin of the phrase 'Hello, World'"
+		text: messages,
 	});
+
 	return c.json(answer);
+})
+.post('/vector', async (c) => {
+
+	const { messages, temperature, topK, topP, maxOutputTokens, responseMimeType, responseSchema } = await c.req.json();
+	if (!messages) return c.json({ error: 'Message is required' }, 400);
+
+	const modelResp = await c.env.AI.run('@cf/baai/bge-small-en-v1.5', {
+		text: messages,
+	});
+
+	let vectors: VectorizeVector[] = [];
+	let id = 1;
+	modelResp.data.forEach((vector) => {
+	  vectors.push({ id: `${id}`, values: vector });
+	  id++;
+	});
+
+	let inserted = await c.env.VECTORIZE.upsert(vectors);
+
+	return c.json(inserted);
 });
 
 export default app;
