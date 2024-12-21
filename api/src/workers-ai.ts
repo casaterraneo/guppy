73b20b0e2f0178f47ed7621d4e38404fba159c1d
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-12-21 19:24:17 +0100

    clean vectore

--- api/src/workers-ai.ts
+++ api/src/workers-ai.ts
@@ -41,26 +41,10 @@ const app = new Hono()
 		text: messages[0],
 	});
 
-	//let results = processItemsInParallel(c, modelResp.data);
-
-	//const vector = await c.env.VECTORIZE.getByIds(['1']);
 	const vector = modelResp.data[0];
-	console.log(vector);
-	//const result = await c.env.VECTORIZE.query(vector[0].values,  { topK: 1 , returnValues: true });
 	const result = await c.env.VECTORIZE.query(vector,  { topK: 1 , returnValues: true });
-	console.log(result.matches);
 
 	return c.json(result.matches);
 });
-
-const processItemsInParallel = async (c, vectors) => {
-	let results = [];
-    await Promise.all(vectors.map(async (vector) => {
-		const result = await c.env.VECTORIZE.query(vector,  { topK: 1 , returnValues: true });
-		results.push(result.matches)
-		console.log(result.matches);
-    }));
-	return results;
-};
 
 export default app;
