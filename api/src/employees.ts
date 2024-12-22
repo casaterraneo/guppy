5afc91a5a048c6448e7034a4dc8940db881ac5c6
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-12-22 20:01:16 +0100

    employees upsert

--- api/src/employees.ts
+++ api/src/employees.ts
@@ -3,21 +3,60 @@ import { Hono } from 'hono';
 // Middleware per verificare i permessi
 function checkPermission(permission) {
 	return (c, next) => {
-	  const user = c.get('user')
-	  if (user && user.permissions && user.permissions.includes(permission)) {
-		return next()
-	  } else {
-		return c.json({ error: 'Insufficient permissions' }, 403)
-	  }
-	}
-  }
+		const user = c.get('user');
+		if (user && user.permissions && user.permissions.includes(permission)) {
+			return next();
+		} else {
+			return c.json({ error: 'Insufficient permissions' }, 403);
+		}
+	};
+}
 
-const app = new Hono()
-	.get('/', checkPermission('read:employees'), async c => {
-		const db = c.get('db');
-		const resp = await db.prepare(`SELECT * FROM [Employee]`).all();
-		return c.json(resp.results);
+const app = new Hono().get('/', checkPermission('read:employees'), async c => {
+	const searchConfigId = c.req.query('searchConfigId');
+	const searchText = c.req.query('searchText');
+
+	const db = c.get('db');
+	const searchConfiguration = await db
+		.prepare(`SELECT * FROM [SearchConfigurations] where Id=?`)
+		.bind(searchConfigId)
+		.first();
+
+	if (searchConfiguration === null) {
+		return c.status(404);
+	}
+
+	const searchFieldResults = await db
+		.prepare(`SELECT Id, ${searchConfiguration.SearchFields} FROM [Employee] order by id`)
+		.all();
+
+	const ids = searchFieldResults.results.map(r => r?.Id) as Array<string>;
+	const names = searchFieldResults.results.map(r => r?.LastName) as Array<string>;
+
+	const modelResp = await c.env.AI.run('@cf/baai/bge-base-en-v1.5', {
+		text: names,
 	});
 
+	let vectors: VectorizeVector[] = [];
+	for (let i = 0; i < modelResp.data.length; i++) {
+		let vector = modelResp.data[i];
+		vectors.push({ id: `${ids[i]}`, values: vector });
+	}
+
+	let inserted = await c.env.VECTORIZE. (vectors);
+
+	console.log(inserted);
+
+	const modelSearch  = await c.env.AI.run('@cf/baai/bge-base-en-v1.5', {
+		text: searchText,
+	});
+
+	const searchResults = await db
+	.prepare(`SELECT * FROM [Employee] where Id=?`)
+		.bind(modelSearch.data[0].id)
+		.first();
+
+	return c.json(searchResults.results);
+});
 
 export default app;
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,55 +1,55 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
-import { logger } from 'hono/logger'
-import { createMiddleware } from 'hono/factory'
-import { createRemoteJWKSet, jwtVerify } from 'jose'
+import { logger } from 'hono/logger';
+import { createMiddleware } from 'hono/factory';
+import { createRemoteJWKSet, jwtVerify } from 'jose';
 import employees from './employees';
 import kvs from './kvs';
 import users from './users';
 import googleAI from './google-ai';
 import workersAI from './workers-ai';
 
-const JWKS = createRemoteJWKSet(new URL('https://dev-lnkfyfu1two0vaem.us.auth0.com/.well-known/jwks.json'))
+const JWKS = createRemoteJWKSet(
+	new URL('https://dev-lnkfyfu1two0vaem.us.auth0.com/.well-known/jwks.json')
+);
 async function verifyToken(token) {
 	const { payload } = await jwtVerify(token, JWKS, {
-	  audience: 'guppy-api',
-	  issuer: 'https://dev-lnkfyfu1two0vaem.us.auth0.com/',
-	})
-	return payload
-  }
+		audience: 'guppy-api',
+		issuer: 'https://dev-lnkfyfu1two0vaem.us.auth0.com/',
+	});
+	return payload;
+}
 
 const tokenValidator = createMiddleware(async (c, next) => {
-
-	const token = c.req.headers.get('Authorization')?.split(' ')[1]
+	const token = c.req.headers.get('Authorization')?.split(' ')[1];
 
 	if (!token) {
-		return c.json({ error: 'No token provided' }, 401)
+		return c.json({ error: 'No token provided' }, 401);
 	}
 
 	try {
-		const payload = await verifyToken(token)
-		c.set('user', payload)
-		return next()
+		const payload = await verifyToken(token);
+		c.set('user', payload);
+		return next();
 	} catch (err) {
-		return c.json({ error: 'Token invalid', message: err.message }, 401)
+		return c.json({ error: 'Token invalid', message: err.message }, 401);
 	}
-})
+});
 
 const dbSetter = createMiddleware(async (c, next) => {
-
 	const user = c.get('user');
 
 	if (user && user.company_name) {
 		var db = c.env.DB;
-		if(c.env.ENVIRONMENT == "production" && user.company_name == "cli") {
+		if (c.env.ENVIRONMENT == 'production' && user.company_name == 'cli') {
 			db = c.env.DB_CLI;
 		}
-		c.set('db', db)
-		return next()
+		c.set('db', db);
+		return next();
 	} else {
-		return c.json({ error: 'No company_name provided' }, 401)
+		return c.json({ error: 'No company_name provided' }, 401);
 	}
-})
+});
 
 const app = new Hono();
 app.use(logger());
@@ -69,10 +69,9 @@ app.onError((err, c) => {
 });
 app.notFound(c => c.text('Not found', 404));
 
-
 export default {
 	fetch: app.fetch,
 	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
-	  //ctx.waitUntil(env.LEADERBOARD_WORKFLOW.create());
+		//ctx.waitUntil(env.LEADERBOARD_WORKFLOW.create());
 	},
-  }
+};
