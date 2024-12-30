71e3b1a9de3f8b31d5a21916627a6aff35077786
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-12-30 16:39:45 +0100

    table Product

--- api/src/workers-ai.ts
+++ api/src/workers-ai.ts
@@ -81,7 +81,7 @@ store. You will take the users questions and turn them into SQL queries using th
 available. Once you have the information you need, you will answer the user's question using
 the data returned. Use listTables to see what tables are present, describeTable to understand
 the schema, and executeQuery to issue an SQL SELECT query.` },
-				{ role: "user", content: "Give to me all names of all tables in the database" },
+				{ role: "user", content: "Give to me the schema of table Product in the database." },
 			],
 			tools: [
 				{
@@ -94,23 +94,23 @@ the schema, and executeQuery to issue an SQL SELECT query.` },
 					  },
 					function: listTables,
 				},
-				// {
-				// 	name: "describeTable",
-				// 	description: `Look up the table schema.
-				// 				Returns:
-				// 				List of columns, where each entry is a tuple of (column name, column type).`,
-				// 	parameters : {
-				// 		type: "object",
-				// 		properties: {
-				// 			tableName: {
-				// 			  type: "string",
-				// 			  description: "The name of the table to describe."
-				// 			}
-				// 		  },
-				// 		  required: ["tableName"]
-				// 	  },
-				// 	function: describeTable,
-				// },
+				{
+					name: "describeTable",
+					description: `Look up the table schema.
+								Returns:
+								List of columns, where each entry is a tuple of (column name, column type).`,
+					parameters : {
+						type: "object",
+						properties: {
+							tableName: {
+							  type: "string",
+							  description: "The name of the table to describe."
+							}
+						  },
+						  required: ["tableName"]
+					  },
+					function: describeTable,
+				},
 				// {
 				// 	name: "executeQuery",
 				// 	description: `Execute a SELECT statement, returning the results.`,
