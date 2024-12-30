2521baae1f46f979bdf8b015f09cfd978c095231
Paolo Terraneo <paolo.terraneo@altersolution.com>
2024-12-30 19:00:35 +0100

    json

--- api/src/workers-ai.ts
+++ api/src/workers-ai.ts
@@ -76,22 +76,15 @@ const app = new Hono()
 			"@hf/nousresearch/hermes-2-pro-mistral-7b",
 			{
 			messages: [
-				{ role: "system", content: `<|im_start|>system
+				{ role: "system", content: `
 You are a function calling AI model.
-You are provided with function signatures within <tools></tools> XML tags.
+You are provided with function signatures within <tools></tools> tags.
 You may call one or more functions to assist with the user query.
 Don't make assumptions about what values to plug into functions.
-Here are the available tools:
-<tools> [
-	{'type': 'function', 'function': {'name': 'listTables', 'description': '', 'parameters': {'type': 'object', 'properties': {}}, 'required': []}},
-	{'type': 'function', 'function': {'name': 'describeTable', 'description': '', 'parameters': {'type': 'object', 'properties': {'tableName': {'type': 'string'}}, 'required': ['tableName']}}},
-	{'type': 'function', 'function': {'name': 'executeQuery', 'description': '', 'parameters': {'type': 'object', 'properties': {'sqlQuery': {'type': 'string'}}, 'required': ['sqlQuery']}}}
-	]
-</tools>
-Use the following pydantic model json schema for each tool call you will make: {'title': 'FunctionCall', 'type': 'object', 'properties': {'arguments': {'title': 'Arguments', 'type': 'object'}, 'name': {'title': 'Name', 'type': 'string'}}, 'required': ['arguments', 'name']} For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows:
-<tool_call>
+Here are the available tools: ['listTables', 'describeTable', 'executeQuery']
+Use the following pydantic model json schema for each tool call you will make: {'title': 'FunctionCall', 'type': 'object', 'properties': {'arguments': {'title': 'Arguments', 'type': 'object'}, 'name': {'title': 'Name', 'type': 'string'}}, 'required': ['arguments', 'name']} For each function call return a json object with function name and arguments within tags as follows:
 {'arguments': <args-dict>, 'name': <function-name>}
-</tool_call><|im_end|>` },
+` },
 				{ role: "user", content: "Who is the youngest employee?" },
 			],
 			tools: [
