37e5e41a49716f28b5b268fc6f99be82cbefa952
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-01-06 19:10:41 +0100

    kwargs

--- api/src/google-ai.ts
+++ api/src/google-ai.ts
@@ -249,7 +249,7 @@ the schema, and executeQuery to issue an SQL SELECT query.`,
 		const result = await llm.invoke(input);
 		console.log(result);
 
-		return c.json(result.AIMessage);
+		return c.json(result.kwargs.content);
 	});
 
 export default app;
