9212cb6d2f3cec0ca2b076ecf9720248afd144ea
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-02-01 10:30:44 +0100

    browser

--- api/src/barista-bot.ts
+++ api/src/barista-bot.ts
@@ -11,25 +11,41 @@ import { z } from 'zod';
 //https://github.com/langchain-ai/langchainjs/blob/main/libs/langchain-cloudflare/src/message_histories.ts
 //https://js.langchain.com/docs/integrations/memory/cloudflare_d1/
 
-const app = new Hono().post('barista-bot', async c => {
-	const { messages } = await c.req.json();
-	if (!messages) return c.json({ error: 'Message is required' }, 400);
-
-	const input = messages[0];
-
-	const db = c.get('db');
-	const memory = new BufferMemory({
-		returnMessages: true,
-		chatHistory: new CloudflareD1MessageHistory({
-			sessionId: 'barista-bot-session-id',
-			database: db,
+// Define your tool
+const fakeBrowserTool = tool(
+	_ => {
+		return 'The search result is xyz...';
+	},
+	{
+		name: 'browser_tool',
+		description: 'Useful for when you need to find something on the web or summarize a webpage.',
+		schema: z.object({
+			url: z.string().describe('The URL of the webpage to search.'),
+			query: z.string().optional().describe('An optional search query to use.'),
 		}),
-	});
+	}
+);
 
-	const prompt = ChatPromptTemplate.fromMessages([
-		[
-			'system',
-			`You are a BaristaBot, an interactive cafe ordering system. A human will talk to you about the
+const app = new Hono()
+	.post('barista-bot', async c => {
+		const { messages } = await c.req.json();
+		if (!messages) return c.json({ error: 'Message is required' }, 400);
+
+		const input = messages[0];
+
+		const db = c.get('db');
+		const memory = new BufferMemory({
+			returnMessages: true,
+			chatHistory: new CloudflareD1MessageHistory({
+				sessionId: 'barista-bot-session-id',
+				database: db,
+			}),
+		});
+
+		const prompt = ChatPromptTemplate.fromMessages([
+			[
+				'system',
+				`You are a BaristaBot, an interactive cafe ordering system. A human will talk to you about the
 available products you have and you will answer any questions about menu items (and only about
 menu items - no off-topic discussion, but you can chat about the products and their history).
 The customer will place an order for 1 or more items from the menu, which you will structure
@@ -46,68 +62,89 @@ You only have the modifiers listed on the menu.
 Once the customer has finished ordering items, Call confirm_order to ensure it is correct then make
 any necessary updates and then call place_order. Once place_order has returned, thank the user and
 say goodbye!`,
-		],
-		new MessagesPlaceholder('history'),
-		['user', '{input}'],
-	]);
+			],
+			new MessagesPlaceholder('history'),
+			['user', '{input}'],
+		]);
 
-	// Define your tool
-	const addToOrderTool = tool(
-		({ drink, modifiers }) => {
-			//return `Adds ${drink} with modifiers: ${modifiers || "no modifiers"}`;
-			return `Adds ${drink}`;
-		},
-		{
-			name: 'add_to_order',
-			description: "Adds the specified drink to the customer's order, including any modifiers.",
-			schema: z.object({
-				drink: z.string().describe('The name of the drink to add to the order.'),
-				modifiers: z.string().optional().describe("An optional description of the modifiers."),
-			}),
-		}
-	);
+		// Define your tool
+		const addToOrderTool = tool(
+			({ drink, modifiers }) => {
+				//return `Adds ${drink} with modifiers: ${modifiers || "no modifiers"}`;
+				return `Adds ${drink}`;
+			},
+			{
+				name: 'add_to_order',
+				description: "Adds the specified drink to the customer's order, including any modifiers.",
+				schema: z.object({
+					drink: z.string().describe('The name of the drink to add to the order.'),
+					modifiers: z.string().optional().describe('An optional description of the modifiers.'),
+				}),
+			}
+		);
 
-	const confirmOrderTool = tool(
-		({drink}) => {
-			return 'Asks the customer to confirm their order.';
-		},
-		{
-			name: 'confirm_order',
-			description: 'Asks the customer if the order is correct.',
-			schema: z.object({
-				drink: z.string().optional().describe("The name of the drink to add to the order.")
-			}),
-		}
-	);
+		const confirmOrderTool = tool(
+			({ drink }) => {
+				return 'Asks the customer to confirm their order.';
+			},
+			{
+				name: 'confirm_order',
+				description: 'Asks the customer if the order is correct.',
+				schema: z.object({
+					drink: z.string().optional().describe('The name of the drink to add to the order.'),
+				}),
+			}
+		);
 
-	const model = new ChatGoogleGenerativeAI({
-		model: 'gemini-1.5-flash-latest',
-		apiKey: c.env.GOOGLE_AI_STUDIO_TOKEN,
-		temperature: 0,
-	}).bindTools([addToOrderTool]);
+		const model = new ChatGoogleGenerativeAI({
+			model: 'gemini-1.5-flash-latest',
+			apiKey: c.env.GOOGLE_AI_STUDIO_TOKEN,
+			temperature: 0,
+		}).bindTools([addToOrderTool]);
 
-	const chain = RunnableSequence.from([
-		{
-			input: initialInput => initialInput.input,
-			memory: () => memory.loadMemoryVariables({}),
-		},
-		{
-			input: previousOutput => previousOutput.input,
-			history: previousOutput => previousOutput.memory.history,
-		},
-		prompt,
-		model,
-		new StringOutputParser(),
-	]);
+		const chain = RunnableSequence.from([
+			{
+				input: initialInput => initialInput.input,
+				memory: () => memory.loadMemoryVariables({}),
+			},
+			{
+				input: previousOutput => previousOutput.input,
+				history: previousOutput => previousOutput.memory.history,
+			},
+			prompt,
+			model,
+			new StringOutputParser(),
+		]);
 
-	const chainInput = { input };
+		const chainInput = { input };
 
-	const res = await chain.invoke(chainInput);
-	await memory.saveContext(chainInput, {
-		output: res,
+		const res = await chain.invoke(chainInput);
+		await memory.saveContext(chainInput, {
+			output: res,
+		});
+
+		return c.json(res);
+	})
+	.post('fake-browser-tool', async c => {
+		const { messages } = await c.req.json();
+		if (!messages) return c.json({ error: 'Message is required' }, 400);
+
+		const input = messages[0];
+
+		const model = new ChatGoogleGenerativeAI({
+			model: 'gemini-1.5-flash-latest',
+			apiKey: c.env.GOOGLE_AI_STUDIO_TOKEN,
+			temperature: 0,
+		}).bindTools([fakeBrowserTool]);
+
+		const res = await model.invoke([
+			[
+				'human',
+				input,
+			],
+		]);
+
+		return c.json(res);
 	});
-
-	return c.json(res);
-});
 
 export default app;
