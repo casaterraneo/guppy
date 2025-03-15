6f91a4187533bfcc0dd5dd35098290047fea0e84
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-03-15 17:29:45 +0100

    MemorySaver

--- api/src/lang-agent.ts
+++ api/src/lang-agent.ts
@@ -1,59 +1,66 @@
 import { Hono } from 'hono';
 
-import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { createReactAgent } from '@langchain/langgraph/prebuilt';
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
-import { tool } from "@langchain/core/tools";
+import { tool } from '@langchain/core/tools';
+import { MemorySaver } from '@langchain/langgraph';
 
-import { z } from "zod";
+import { z } from 'zod';
 
-const search = tool(async ({ query }) => {
-	if (query.toLowerCase().includes("sf") || query.toLowerCase().includes("san francisco")) {
-	  return "It's 60 degrees and foggy."
+const search = tool(
+	async ({ query }) => {
+		if (query.toLowerCase().includes('sf') || query.toLowerCase().includes('san francisco')) {
+			return "It's 60 degrees and foggy.";
+		}
+		return "It's 90 degrees and sunny.";
+	},
+	{
+		name: 'search',
+		description: 'Call to surf the web.',
+		schema: z.object({
+			query: z.string().describe('The query to use in your search.'),
+		}),
 	}
-	return "It's 90 degrees and sunny."
-  }, {
-	name: "search",
-	description: "Call to surf the web.",
-	schema: z.object({
-	  query: z.string().describe("The query to use in your search."),
-	}),
-  });
+);
 
-const app = new Hono()
-	.post('run', async c => {
-		const { messages } = await c.req.json();
-		if (!messages)
-			return c.json({ error: 'Message is required' }, 400);
+const app = new Hono().post('run', async c => {
+	const { messages } = await c.req.json();
+	if (!messages) return c.json({ error: 'Message is required' }, 400);
 
-		const input = messages[0];
+	const input = messages[0];
 
-		//https://ai.google.dev/gemini-api/docs/models/gemini
-		//gemini-pro
-		//model: 'gemini-1.5-flash-latest',
-		const model = new ChatGoogleGenerativeAI({
-			model: 'gemini-2.0-flash',
-			apiKey: c.env.GOOGLE_AI_STUDIO_TOKEN,
-			temperature: 0,
-		});
-
-		const agent = createReactAgent({
-			llm: model,
-			tools: [search],
-		  });
-
-		  const output = await agent.invoke(
-			{
-			  messages: [{
-				role: "user",
-				content: input
-			  }]
-			}
-		  );
-
-		  console.log(output.messages);
-
-		  //return c.json(result.content);
-		  return c.json(output.messages.map(msg => msg.content));
+	//https://ai.google.dev/gemini-api/docs/models/gemini
+	//gemini-pro
+	//model: 'gemini-1.5-flash-latest',
+	const model = new ChatGoogleGenerativeAI({
+		model: 'gemini-2.0-flash',
+		apiKey: c.env.GOOGLE_AI_STUDIO_TOKEN,
+		temperature: 0,
 	});
+
+	const agentCheckpointer = new MemorySaver();
+
+	const agent = createReactAgent({
+		llm: model,
+		tools: [search],
+		checkpointSaver: agentCheckpointer,
+	});
+
+	const output1 = await agent.invoke(
+		{ messages: [{ role: 'user', content: input }] },
+		{ configurable: { thread_id: '42' } }
+	);
+
+	console.log(output1.messages);
+
+	const output2 = await agent.invoke(
+		{ messages: [{ role: 'user', content: "what about ny" }] },
+		{ configurable: { thread_id: '42' } }
+	);
+
+	console.log(output2.messages);
+
+	return c.json(output2.messages.map(msg => msg.content));
+});
 
 export default app;
