fd575d66de2a1de90b1dbde776e959ee84c023ef
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-03-22 19:11:16 +0100

    memorySaver

--- api/src/D1Checkpointer.ts
+++ api/src/D1Checkpointer.ts
@@ -1,4 +1,27 @@
 import { BaseCheckpointSaver } from '@langchain/langgraph-checkpoint';
+import { MemorySaver } from '@langchain/langgraph';
 
-export class D1Checkpointer extends BaseCheckpointSaver {
-}
+export class D1Checkpointer extends BaseCheckpointSaver  {
+	private memorySaver: MemorySaver;
+
+	constructor() {
+	  super();
+	  this.memorySaver = new MemorySaver();
+	}
+
+	async put(threadId: string, threadTs: number, data: any): Promise<void> {
+	  return this.memorySaver.put(threadId, threadTs, data);
+	}
+
+	async putWrites(threadId: string, threadTs: number, writes: any[]): Promise<void> {
+	  return this.memorySaver.putWrites(threadId, threadTs, writes);
+	}
+
+	async getTuple(threadId: string, threadTs: number): Promise<any | null> {
+	  return this.memorySaver.getTuple(threadId, threadTs);
+	}
+
+	async list(filter?: Partial<{ threadId: string; threadTs: number }>): Promise<any[]> {
+	  return this.memorySaver.list(filter);
+	}
+  }
