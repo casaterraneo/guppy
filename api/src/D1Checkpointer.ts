8028776d16c032926e5cf93f8b7b9144a9da3a9b
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-03-22 19:26:12 +0100

    memorySaver

--- api/src/D1Checkpointer.ts
+++ api/src/D1Checkpointer.ts
@@ -1,29 +1,34 @@
 import { BaseCheckpointSaver } from '@langchain/langgraph-checkpoint';
 import { MemorySaver } from '@langchain/langgraph';
-import type { D1Database } from "@cloudflare/workers-types";
+import type { D1Database } from '@cloudflare/workers-types';
 
-export class D1Checkpointer extends BaseCheckpointSaver  {
+export class D1Checkpointer extends BaseCheckpointSaver {
 	private memorySaver: MemorySaver;
 	private db: D1Database;
 
 	constructor(db: D1Database) {
 		super();
 		this.db = db;
-	  }
+		this.memorySaver = new MemorySaver();
+	}
 
 	async put(threadId: string, threadTs: number, data: any): Promise<void> {
-	  return this.memorySaver.put(threadId, threadTs, data);
+		console.log(`D1Checkpointer put ${threadId}`);
+		return this.memorySaver.put(threadId, threadTs, data);
 	}
 
 	async putWrites(threadId: string, threadTs: number, writes: any[]): Promise<void> {
-	  return this.memorySaver.putWrites(threadId, threadTs, writes);
+		console.log(`D1Checkpointer putWrites ${threadId}`);
+		return this.memorySaver.putWrites(threadId, threadTs, writes);
 	}
 
 	async getTuple(threadId: string, threadTs: number): Promise<any | null> {
-	  return this.memorySaver.getTuple(threadId, threadTs);
+		console.log(`D1Checkpointer getTuple ${threadId}`);
+		return this.memorySaver.getTuple(threadId, threadTs);
 	}
 
 	async list(filter?: Partial<{ threadId: string; threadTs: number }>): Promise<any[]> {
-	  return this.memorySaver.list(filter);
+		console.log(`D1Checkpointer list ${filter}`);
+		return this.memorySaver.list(filter);
 	}
-  }
+}
