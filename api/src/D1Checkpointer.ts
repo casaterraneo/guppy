a53d83ccd980febb23c9c9590ba6d70c3294adba
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-03-23 18:00:45 +0100

    checkpoint_id

--- api/src/D1Checkpointer.ts
+++ api/src/D1Checkpointer.ts
@@ -73,13 +73,14 @@ export class D1Checkpointer extends BaseCheckpointSaver {
 
 		await this.db
 			.prepare(
-				`INSERT OR REPLACE INTO checkpoints (thread_id, checkpoint_ns, checkpoint_id, checkpoint, metadata) VALUES (?, ?, ?, ?, ?)`
+				`INSERT OR REPLACE INTO checkpoints (thread_id, checkpoint_ns, checkpoint_id, parent_checkpoint_id, checkpoint, metadata) VALUES (?, ?, ?, ?, ?, ?)`
 			)
 			//.bind(...row)
 			.bind(
 				config.configurable?.thread_id,
 				config.configurable?.checkpoint_ns || '',
 				checkpoint.id,
+				config.configurable?.checkpoint_id,
 				serializedCheckpoint,
 				serializedMetadata
 			)
