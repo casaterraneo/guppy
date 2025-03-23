dcda46dfb1ed6c313bc1a6ad8147c3e6e416be0a
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-03-23 16:45:22 +0100

    getTuple

--- api/src/D1Checkpointer.ts
+++ api/src/D1Checkpointer.ts
@@ -4,6 +4,7 @@ import type { RunnableConfig } from '@langchain/core/runnables';
 import {
 	BaseCheckpointSaver,
 	type Checkpoint,
+	type CheckpointTuple,
 	type CheckpointMetadata
 } from '@langchain/langgraph-checkpoint';
 
@@ -29,18 +30,6 @@ export class D1Checkpointer extends BaseCheckpointSaver {
 	): Promise<RunnableConfig> {
 		console.log(`D1Checkpointer put`);
 
-		// await this.db.exec(`
-		// 	CREATE TABLE IF NOT EXISTS checkpoints (
-		// 	  thread_id TEXT NOT NULL,
-		// 	  checkpoint_ns TEXT NOT NULL DEFAULT '',
-		// 	  checkpoint_id TEXT NOT NULL,
-		// 	  parent_checkpoint_id TEXT,
-		// 	  type TEXT,
-		// 	  checkpoint BLOB,
-		// 	  metadata BLOB,
-		// 	  PRIMARY KEY (thread_id, checkpoint_ns, checkpoint_id)
-		// 	);
-		//   `);
 		await this.db.exec(`CREATE TABLE IF NOT EXISTS checkpoints (thread_id TEXT NOT NULL, checkpoint_ns TEXT NOT NULL DEFAULT '', checkpoint_id TEXT NOT NULL, parent_checkpoint_id TEXT, type TEXT, checkpoint BLOB, metadata BLOB, PRIMARY KEY (thread_id, checkpoint_ns, checkpoint_id));`);
 
 		return this.memorySaver.put(config, checkpoint, metadata);
@@ -51,9 +40,9 @@ export class D1Checkpointer extends BaseCheckpointSaver {
 		return this.memorySaver.putWrites(threadId, threadTs, writes);
 	}
 
-	async getTuple(threadId: string, threadTs: number): Promise<any | null> {
-		console.log(`D1Checkpointer getTuple ${threadId}`);
-		return this.memorySaver.getTuple(threadId, threadTs);
+	async getTuple(config: RunnableConfig): Promise<CheckpointTuple | undefined> {
+		console.log(`D1Checkpointer getTuple`);
+		return this.memorySaver.getTuple(config);
 	}
 
 	async list(filter?: Partial<{ threadId: string; threadTs: number }>): Promise<any[]> {
