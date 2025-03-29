1c6ad75c0c7fee6b6b09598a194361cbfe8940dc
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-03-29 10:44:30 +0100

    run react

--- api/src/lang-agent.ts
+++ api/src/lang-agent.ts
@@ -7,8 +7,15 @@ import { MemorySaver } from '@langchain/langgraph';
 
 import { z } from 'zod';
 
-import type { BaseMessage, BaseMessageLike } from '@langchain/core/messages';
+import type {
+	BaseMessage,
+	BaseMessageLike,
+	ToolMessage,
+	AIMessage,
+	isAIMessage,
+} from '@langchain/core/messages';
 import { addMessages, entrypoint, task, getPreviousState } from '@langchain/langgraph';
+import { type ToolCall } from '@langchain/core/messages/tool';
 
 import { D1Checkpointer } from './D1Checkpointer';
 
@@ -121,6 +128,104 @@ const app = new Hono()
 			console.log('='.repeat(30), `${chunk.getType()} message`, '='.repeat(30));
 			console.log(chunk.content);
 			content = chunk.content;
+		}
+
+		return c.json(content);
+	})
+	.post('run-react', async c => {
+		const { messages } = await c.req.json();
+		if (!messages) return c.json({ error: 'Message is required' }, 400);
+
+		const input = messages[0];
+
+		const model = new ChatGoogleGenerativeAI({
+			model: 'gemini-2.0-flash',
+			apiKey: c.env.GOOGLE_AI_STUDIO_TOKEN,
+			temperature: 0,
+		});
+
+		const getWeather = tool(
+			async ({ location }) => {
+				const lowercaseLocation = location.toLowerCase();
+				if (lowercaseLocation.includes('sf') || lowercaseLocation.includes('san francisco')) {
+					return "It's sunny!";
+				} else if (lowercaseLocation.includes('boston')) {
+					return "It's rainy!";
+				} else {
+					return `I am not sure what the weather is in ${location}`;
+				}
+			},
+			{
+				name: 'getWeather',
+				schema: z.object({
+					location: z.string().describe('location to get the weather for'),
+				}),
+				description: 'Call to get the weather from a specific location.',
+			}
+		);
+
+		const tools = [getWeather];
+
+		const toolsByName = Object.fromEntries(tools.map(tool => [tool.name, tool]));
+
+		const callModel = task('callModel', async (messages: BaseMessageLike[]) => {
+			const response = await model.bindTools(tools).invoke(messages);
+			return response;
+		});
+
+		const callTool = task('callTool', async (toolCall: ToolCall): Promise<AIMessage> => {
+			const tool = toolsByName[toolCall.name];
+			//const observation = await tool.invoke(toolCall.args);
+			//return new ToolMessage({ content: observation, tool_call_id: toolCall.id });
+			// Can also pass toolCall directly into the tool to return a ToolMessage
+			return tool.invoke(toolCall);
+		});
+
+		const agent = entrypoint('agent', async (messages: BaseMessageLike[]) => {
+			let currentMessages = messages;
+			let llmResponse = await callModel(currentMessages);
+			while (true) {
+				if (!llmResponse.tool_calls?.length) {
+					break;
+				}
+				// Execute tools
+				const toolResults = await Promise.all(
+					llmResponse.tool_calls.map(toolCall => {
+						return callTool(toolCall);
+					})
+				);
+				// Append to message list
+				currentMessages = addMessages(currentMessages, [llmResponse, ...toolResults]);
+				// Call model again
+				llmResponse = await callModel(currentMessages);
+			}
+			return llmResponse;
+		});
+
+		const prettyPrintMessage = (message: BaseMessage) => {
+			console.log('='.repeat(30), `${message.getType()} message`, '='.repeat(30));
+			console.log(message.content);
+			if (isAIMessage(message) && message.tool_calls?.length) {
+				console.log(JSON.stringify(message.tool_calls, null, 2));
+			}
+		};
+
+		// Usage example
+		const userMessage = { role: 'user', content: input };
+		console.log(userMessage);
+
+		const stream = await agent.stream([userMessage]);
+
+		let content = '';
+		for await (const step of stream) {
+			for (const [taskName, update] of Object.entries(step)) {
+				const message = update as BaseMessage;
+				// Only print task updates
+				if (taskName === 'agent') continue;
+				console.log(`\n${taskName}:`);
+				prettyPrintMessage(message);
+				content = message;
+			}
 		}
 
 		return c.json(content);
