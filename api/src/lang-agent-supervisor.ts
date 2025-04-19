0dc70b0368f583628446fedd9c56ee8538900f25
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-04-19 15:32:39 +0200

    package.json

--- api/src/lang-agent-supervisor.ts
+++ api/src/lang-agent-supervisor.ts
@@ -90,7 +90,7 @@ const app = new Hono().post('run-agent-supervisor', async c => {
 	const workflow = createSupervisor({
 		agents: [researchAgent, mathAgent],
 		llm: model,
-		outputMode: "last_message",
+		outputMode: "last_message", // default
 		//outputMode: "full_history",
 		prompt:
 			'You are a team supervisor managing a research expert and a math expert. ' +
