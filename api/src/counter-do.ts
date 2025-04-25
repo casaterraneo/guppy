aa3bd8cf379c291dc730255166214937f4f46159
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-04-25 11:54:29 +0200

    c.json(counterValue)

--- api/src/counter-do.ts
+++ api/src/counter-do.ts
@@ -6,21 +6,21 @@ const app = new Hono()
 		const id = env.COUNTER.idFromName('counter');
 		const stub = env.COUNTER.get(id);
 		const counterValue = await stub.getCounterValue();
-		return c.json({ value: counterValue });
+		return c.json(counterValue);
 	})
 	.post('/increment', async c => {
 		const env = c.env;
 		const id = env.COUNTER.idFromName('counter');
 		const stub = env.COUNTER.get(id);
 		const counterValue = await stub.increment();
-		return c.json({ value: counterValue });
+		return c.json(counterValue);
 	})
 	.post('/decrement', async c => {
 		const env = c.env;
 		const id = env.COUNTER.idFromName('counter');
 		const stub = env.COUNTER.get(id);
 		const counterValue = await stub.decrement();
-		return c.json({ value: counterValue });
+		return c.json(counterValue);
 	});
 
 export default app;
