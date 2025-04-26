b589b22943f57a5391edc519d2f81dfa5524a974
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-04-26 16:21:40 +0200

    WebSocketPair

--- api/src/DurableObject/receiver.ts
+++ api/src/DurableObject/receiver.ts
@@ -1,15 +1,38 @@
-import { DurableObject } from "cloudflare:workers";
+import { DurableObject } from 'cloudflare:workers';
 
 // 1. Make sure the class name corresponds exactly with the one
 // added in wrangler.toml earlier
 export class WebhookReceiver extends DurableObject {
-  constructor(ctx: DurableObjectState, env: CloudflareBindings) {
-    super(ctx, env);
-  }
-  // 2. This fetch method serves as a communication layer between the Worker
-  // and the Durable Object
-  async fetch(request: Request) {
-	console.log(`Message from client: ${request}`);
-    return new Response("Hello world from a Durable Object");
-  }
+	constructor(ctx: DurableObjectState, env: CloudflareBindings) {
+		super(ctx, env);
+	}
+	// 2. This fetch method serves as a communication layer between the Worker
+	// and the Durable Object
+	async fetch(request: Request) {
+		console.log(`Message from client: ${request}`);
+
+		const webSocketPair = new WebSocketPair();
+		const [client, server] = Object.values(webSocketPair);
+		this.ctx.acceptWebSocket(server);
+
+		//return new Response("Hello world from a Durable Object");
+		return new Response(null, {
+			status: 101,
+			webSocket: client,
+		});
+	}
+
+	async webSocketMessage(ws, message) {
+		// Upon receiving a message from the client, reply with the same message,
+		// but will prefix the message with "[Durable Object]: " and return the
+		// total number of connections.
+		ws.send(
+			`[Durable Object] message: ${message}, connections: ${this.ctx.getWebSockets().length}`
+		);
+	}
+
+	async webSocketClose(ws, code, reason, wasClean) {
+		// If the client closes the connection, the runtime will invoke the webSocketClose() handler.
+		ws.close(code, 'Durable Object is closing WebSocket');
+	}
 }
--- api/src/test-ws-do.ts
+++ api/src/test-ws-do.ts
@@ -1,9 +1,14 @@
 import { Hono } from 'hono';
 
-const app = new Hono().get("/", async (c) => {
-	const id = c.env.WEBHOOK_RECEIVER.idFromName("default");
+const app = new Hono().get('/', async c => {
+	if (c.req.header('upgrade') !== 'websocket') {
+		return c.text('Expected Upgrade: websocket', 426);
+	}
+
+	const id = c.env.WEBHOOK_RECEIVER.idFromName('default');
 	const stub = c.env.WEBHOOK_RECEIVER.get(id);
+
 	return stub.fetch(c.req.raw);
-  });
+});
 
 export default app;
