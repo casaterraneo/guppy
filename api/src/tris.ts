a9d3cf126180cfed9435b51f46e0466008ed0724
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-05-02 11:46:39 +0200

    username

--- api/src/tris.ts
+++ api/src/tris.ts
@@ -5,15 +5,15 @@ const app = new Hono().get('/', async c => {
 		return c.text('Expected Upgrade: websocket', 426);
 	}
 
-	const user = c.get('user');
-	console.log('[user.name for DO]', user?.name);
+	const username = c.req.query('username');
+
+	console.log('[user.name for DO]', username);
 	if (!user) {
 		console.log('No user found');
 		return c.text('No user found', 401);
 	}
-	console.log('[user.name for DO]', user.name);
 
-	const id = c.env.TRIS_RECEIVER.idFromName(user.name);
+	const id = c.env.TRIS_RECEIVER.idFromName(username);
 	const stub = c.env.TRIS_RECEIVER.get(id);
 
 	return stub.fetch(c.req.raw);
