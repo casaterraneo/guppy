8054c3290c32b304a07f1083b79b36cfea9200ea
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-05-10 13:04:49 +0200

    game board

--- api/src/Papi/Game.ts
+++ api/src/Papi/Game.ts
@@ -4,4 +4,5 @@ export class Game {
     gameId: string = '';
     playerList: Player[] = [];
 	itemList: string[] = ["X", "X", "X", "X", "X", "O", "O", "O", "O"];
+	board: string[] = new Array(9).fill("");
 }
--- blazor/Papi/Game.cs
+++ blazor/Papi/Game.cs
@@ -5,4 +5,5 @@ public class Game
     public string GameId { get; set; } = string.Empty;
     public List<Player> PlayerList { get; set; } = [];
     public List<string> ItemList { get; set; } = [];
+    public string[] Board = [];
 }    
