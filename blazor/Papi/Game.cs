1a8efe411f42c4ccaf9bd8b053caca5d5a9dc0d5
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-05-11 19:46:02 +0200

    CheckWinOrDraw

--- blazor/Papi/Game.cs
+++ blazor/Papi/Game.cs
@@ -4,14 +4,46 @@ namespace Blazor.Papi;
 
 public class Game
 {
+    public enum GamePhase { Started, Finished }
+
     public string GameId { get; set; } = string.Empty;
     public List<Player> PlayerList { get; set; } = [];
     public List<string> ItemList { get; set; } = [];
     public string[] Board = [];
-    
+
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public GamePhase Phase { get; set; } = GamePhase.Started;
+    
+    public string? GameResult { get; set; }
 
-    public enum GamePhase { Started, Finished }
+    public void CheckWinOrDraw()
+    {
+        int[,] wins = new int[,] {
+            {0,1,2}, {3,4,5}, {6,7,8},
+            {0,3,6}, {1,4,7}, {2,5,8},
+            {0,4,8}, {2,4,6}
+        };
+
+        foreach (var symbol in new[] { "X", "O" })
+        {
+            for (int i = 0; i < wins.GetLength(0); i++)
+            {
+                if (Board[wins[i,0]] == symbol &&
+                    Board[wins[i,1]] == symbol &&
+                    Board[wins[i,2]] == symbol)
+                {
+                    GameResult = symbol;
+                    Phase = GamePhase.Finished;
+                    return;
+                }
+            }
+        }
+
+        if (Board.All(cell => !string.IsNullOrEmpty(cell)))
+        {
+            GameResult = "Draw";
+            Phase = GamePhase.Finished;
+        }
+    }    
 }
 
