abf350edb25dabdad6fd0c56e34d771ca4137859
Paolo Terraneo <paolo.terraneo@altersolution.com>
2025-05-18 12:41:57 +0200

    gameMode remote

--- api/src/Papi/GameFactory.ts
+++ api/src/Papi/GameFactory.ts
@@ -3,20 +3,52 @@ import { Player } from './Player';
 
 export class GameFactory {
 	static create(userName: string, gameMode: GameMode): Game {
-		const player1 = new Player();
-		player1.playerId = 'X';
-		player1.name = userName;
-
-		const player2 = new Player();
-		player2.playerId = 'O';
-		player2.name = userName;
-
 		const game = new Game();
-		game.gameId = `${player1.name}|${player2.name}`;
 		game.gameMode = gameMode;
-		game.gamePhase = 'start';
-		game.playerList.push(player1, player2);
 
-		return game;
+		if (gameMode === 'local') {
+			const player1 = new Player();
+			player1.playerId = 'X';
+			player1.name = userName;
+
+			const player2 = new Player();
+			player2.playerId = 'O';
+			player2.name = userName;
+
+			game.gameId = `${player1.name}|${player2.name}`;
+			game.gamePhase = 'start';
+			game.playerList.push(player1, player2);
+
+			return game;
+		}
+
+		if (gameMode === 'remote' && userName.includes("player1")) {
+			const player1 = new Player();
+			player1.playerId = 'X';
+			player1.name = userName;
+
+			game.gameId = "player1@mail.com|player2@mail.com";
+			game.gamePhase = 'init';
+			game.playerList.push(player1);
+
+			return game;
+		}
+
+		if (gameMode === 'remote' && userName.includes("player2")) {
+			const player1 = new Player();
+			player1.playerId = 'X';
+			player1.name = "player1@mail.com";
+
+			const player2 = new Player();
+			player2.playerId = 'O';
+			player2.name = userName;
+
+			game.gameId = "player1@mail.com|player2@mail.com";
+			game.gamePhase = 'start';
+			game.playerList.push(player1, player2);
+
+			return game;
+		}
+		throw new Error('Invalid game mode');
 	}
 }
